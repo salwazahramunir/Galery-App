ac01138b6d8657592775e412812616f52b07b21d
salwazahramunir <salwazahramunir@gmail.com>
2024-10-12 07:36:56 +0700

    challenge done

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,5 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
-const studentName = "YOURNAME" // TODO: replace this
-const studentId = "YOURSTUDENTID" // TODO: replace this
+const url = "https://ruang-galery-app.netlify.app/" // TODO: replace this
+const studentName = "Salwa Az-Zahra Munir" // TODO: replace this
+const studentId = "FS10643018" // TODO: replace this
 
 export { url, studentName, studentId }
